34a8cd5abec3d993b4c9d05f003405283096fbe8
unknown <sharjoonibnuhussain99@gmail.com>
2023-07-06 22:45:30 +0530

    binded frontend

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app =  express();
 const cors = require('cors')
 const cookieParcer = require('cookie-parser')
+const path =require("path");
 
 app.use(cors({}));
 app.use(cookieParcer());
@@ -11,7 +12,10 @@ app.use(express.json());
 app.use('/api/user/auth/',usersRoute);
 app.use('/admin/',adminRoute);
 
-
+app.use(express.static(path.join(__dirname,'../FrontEnd/eventspot/build')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../FrontEnd/eventspot/build/index.html'));
+})
 
 
 module.exports =app;
